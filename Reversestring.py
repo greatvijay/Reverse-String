6824fde634e7a7636275cb453ca3c2c6770d5231
greatvijay <136738331+greatvijay@users.noreply.github.com>
2023-06-28 21:59:51 +0530

    Add files via upload

---
+++ Reversestring.py
@@ -0,0 +1,7 @@
+#Program to reverse string
+string=input("Enter the string :")
+r_str=""
+for i in string:
+    r_str=i+r_str #conacatination of string
+print("Entered string :",string) 
+print("Reversed string :",r_str) 
